e72c93d1db4a9d0e71f167d8e86529d8b37197d6
Huijie Liu <1627158922@qq.com>
2024-05-27 21:58:43 +0800

    Change nvim config (from craftzdog)

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,2 @@
-require("jay.core")
-require("jay.lazy")
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
--- .config/nvim/lua/jay/core/init.lua
+++
@@ -1,6 +0,0 @@
-require("jay.core.options")
-require("jay.core.keymaps")
-
-colorscheme = function()
-  require("cyberdream").load()
-end
