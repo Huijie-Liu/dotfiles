457cd2e41deadfae9daf8b2d9bf3aecd4d326e07
Huijie Liu <1627158922@qq.com>
2024-05-27 14:35:56 +0800

    formatting

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,7 @@
 # Homebrew
 eval (/opt/homebrew/bin/brew shellenv)
 
-# aliases
+# ----- aliases -----
 alias proxy="export all_proxy=http://127.0.0.1:7890 && echo 代理成功"
 alias ydl="youtube-dl"
 alias dw="download_wallpaper"
@@ -13,6 +13,31 @@ alias python="python3"
 
 # bind clear-screen shortcut from \cl to \el
 bind --preset \el clear-screen
+
+
+# ----- starship -----
+starship init fish | source
+
+
+# ----- fzf -----
+fzf_configure_bindings --directory=\ct --git_status=\cgs --git_log=\cgl --history=\cr --processes=\cp
+set fzf_preview_dir_cmd eza --all --color=always
+set fzf_fd_opts --exclude .git --hidden --max-depth 5
+
+
+# ----- Eza (better ls) -----
+alias ls="eza --color=always --long --git --no-filesize --icons=always --no-time --no-user --no-permissions"
+alias l="ls -la"
+
+
+# ----- TheFuck tool -----
+thefuck --alias | source
+thefuck --alias fk | source
+
+
+# ----- Zoxide (better cd) -----
+zoxide init fish | source
+alias cd="z"
 
 
 # >>> conda initialize >>>
@@ -29,26 +54,3 @@ end
 # <<< conda initialize <<<
 
 
-# -----starship-----
-starship init fish | source
-
-
-# -----fzf-----
-fzf_configure_bindings --directory=\ct --git_status=\cgs --git_log=\cgl --history=\cr --processes=\cp
-set fzf_preview_dir_cmd eza --all --color=always
-set fzf_fd_opts --exclude .git --hidden --max-depth 5
-
-
-# ---- Eza (better ls) -----
-alias ls="eza --color=always --long --git --no-filesize --icons=always --no-time --no-user --no-permissions"
-alias l="ls -la"
-
-
-# -----TheFuck tool-----
-thefuck --alias | source
-thefuck --alias fk | source
-
-
-# ---- Zoxide (better cd) ----
-zoxide init fish | source
-alias cd="z"
