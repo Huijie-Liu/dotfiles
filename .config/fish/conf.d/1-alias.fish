bfe671f027fca14c8881d5b62493fa9907fcf8e8
Huijie Liu <1627158922@qq.com>
2024-12-28 18:00:46 +0800

    ♻️ Refine config details

--- .config/fish/conf.d/1-alias.fish
+++ .config/fish/conf.d/1-alias.fish
@@ -4,11 +4,13 @@ alias x="exit"
 alias e="nvim ~/.config/fish/config.fish"
 alias r="source ~/.config/fish/config.fish"
 alias v="nvim"
-alias zj="zellij"
 
 # 文件管理
 alias ls="eza --color=always --long --git --no-filesize --icons=always --no-time --no-user --no-permissions"
+alias la="eza --color=always --long --git --icons=always"
+alias laa="eza --color=always --long --git --icons=always -la"
 alias l="ls -la"
+alias lt="ls --tree --level 2"
 alias cd="z"
 alias md="mkdir -p"
 alias rd="rmdir"
@@ -17,10 +19,18 @@ alias rd="rmdir"
 alias h="history -10" # 最近10条历史记录
 alias hg="history | grep " # 搜索历史记录
 
-# Tmux 控制
+# Tmux
 alias ta="tmux attach"
 alias tl="tmux list-session"
 alias ts="tmux new-session"
+
+# zellij
+alias zj="zellij"
+alias za="zellij attach"
+alias zl="zellij list-sessions"
+alias zs="zellij --session"
+alias zk="zellij kill-session"
+alias zka="zellij kill-all-sessions"
 
 # 系统工具
 alias proxy="export https_proxy=http://127.0.0.1:7890 http_proxy=http://127.0.0.1:7890 all_proxy=socks5://127.0.0.1:7890"
@@ -30,6 +40,5 @@ alias python="python3"
 alias lg="lazygit"
 
 # 其他工具
-alias ydl="youtube-dl"
 alias dw="download_wallpaper"
 alias ff="fastfetch"
