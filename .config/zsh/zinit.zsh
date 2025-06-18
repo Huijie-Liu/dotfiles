a7f93ed240df99d6dae554a3920d05898fde14f6
huijieliu <huijieliu@buaa.edu.cn>
2025-06-18 04:47:17 +0000

    ♻️ Clean zinit zsh config

--- .config/zsh/zinit.zsh
+++ .config/zsh/zinit.zsh
@@ -1,24 +1,15 @@
-# Zinit 安装检查
-if [[ ! -f $HOME/.local/share/zinit/zinit.git/zinit.zsh ]]; then
-    print -P "%F{33} %F{220}Installing zinit...%f"
-    command mkdir -p "$HOME/.local/share/zinit" && command chmod g-rwX "$HOME/.local/share/zinit"
-    command git clone https://github.com/zdharma-continuum/zinit "$HOME/.local/share/zinit/zinit.git"
-fi
+ZINIT_HOME="${XDG_DATA_HOME:-${HOME}/.local/share}/zinit/zinit.git"
+[ ! -d $ZINIT_HOME ] && mkdir -p "$(dirname $ZINIT_HOME)"
+[ ! -d $ZINIT_HOME/.git ] && git clone https://github.com/zdharma-continuum/zinit.git "$ZINIT_HOME"
+source "${ZINIT_HOME}/zinit.zsh"
 
-source "$HOME/.local/share/zinit/zinit.git/zinit.zsh"
+# zinit auto completions
 autoload -Uz _zinit
 (( ${+_comps} )) && _comps[zinit]=_zinit
 
-# 延迟加载插件
-zinit ice wait'0' lucid
-zinit light zsh-users/zsh-autosuggestions
-
-zinit ice wait'1' lucid
-zinit light zsh-users/zsh-completions
-
-# syntax-highlighting 应该最后加载
-zinit ice wait'2' lucid
-zinit light zsh-users/zsh-syntax-highlighting
-
-# 加载 oh-my-zsh 的 git 插件
-zinit snippet OMZ::plugins/git/git.plugin.zsh
+# zsh plugins
+zinit lucid for \
+  light-mode zsh-users/zsh-autosuggestions \
+  light-mode zsh-users/zsh-completions \
+  light-mode zsh-users/zsh-syntax-highlighting \
+  OMZP::git
