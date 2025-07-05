a76e773e1bace13f3c24822d4fc137212384665a
liuhuijie <huijieliu@buaa.edu.cn>
2025-07-05 22:16:03 +0800

    update snacks.nvim config

--- .config/nvim/lua/plugins/core.lua
+++ .config/nvim/lua/plugins/core.lua
@@ -1,18 +1,46 @@
+
 return {
-  -- snacks core functionality
   {
     "folke/snacks.nvim",
     priority = 1000,
     lazy = false,
+
     opts = {
-      bigfile = { enabled = true },
-      indent = { enabled = true },
-      input = { enabled = true },
-      notifier = { enabled = true },
-      quickfile = { enabled = true },
+      bigfile      = { enabled = true },
+      indent       = { enabled = true },
+      input        = { enabled = true },
+      notifier     = { enabled = true },
+      quickfile    = { enabled = true },
       statuscolumn = { enabled = true },
-      words = { enabled = true },
-      scroll = { enabled = false },
+      words        = { enabled = true },
+      scroll       = { enabled = false },
+
+      picker = {
+        win = {
+          input = {
+            keys = {
+              ["<Esc>"] = { "close", mode = { "n", "i" } },
+            },
+          },
+        },
+        sources = {
+          files = {
+            hidden  = true,
+            follow  = true,
+          },
+        },
+      },
+    },
+
+    keys = {
+      {
+        "<leader>fa",
+        function()
+          Snacks.picker.files({ hidden = true, ignored = true })
+        end,
+        desc = "Find All Files (cwd)",
+      },
     },
   },
 }
+
