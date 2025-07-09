a2c7181ee1f7aebb4b5401d4b04905d97490ce59
liuhuijie <huijieliu@buaa.edu.cn>
2025-07-09 13:14:49 +0800

    change vscode nvim breakpoint keybind

--- .config/nvim/lua/config/vscode.lua
+++ .config/nvim/lua/config/vscode.lua
@@ -8,9 +8,8 @@ keymap.set("n", "<leader><leader>", function() vscode.action("workbench.action.q
 keymap.set("n", "<leader>e", function() vscode.action("revealInExplorer") end, { desc = "Reveal in Explorer" })
 keymap.set("n", "<leader>r", function() vscode.action("editor.action.rename") end, { desc = "Rename Symbol" })
 
--- Debug and terminal
-keymap.set("n", "<leader>b", function() vscode.action("editor.debug.action.toggleBreakpoint") end, { desc = "Toggle Breakpoint" })
-keymap.set("n", "<leader>t", function() vscode.action("workbench.action.terminal.toggleTerminal") end, { desc = "Toggle Terminal" })
+-- Debug
+keymap.set("n", "<leader>.", function() vscode.action("editor.debug.action.toggleBreakpoint") end, { desc = "Toggle Breakpoint" })
 
 -- Split management
 keymap.set("n", "<leader>v", function() vscode.action("workbench.action.splitEditorRight") end, { desc = "Split Right" })
