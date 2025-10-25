070a9854fb6b19c2ff693f44aecc35f062e37b37
Huijie Liu <hjliu.jay@qq.com>
2025-10-25 12:02:40 +0800

    remove mamba & update ghostty

--- .config/fish/conf.d/mamba.fish
+++
@@ -1,6 +0,0 @@
-# >>> mamba initialize >>>
-# !! Contents within this block are managed by 'mamba shell init' !!
-set -gx MAMBA_EXE /opt/homebrew/bin/mamba
-set -gx MAMBA_ROOT_PREFIX "$HOME/.local/share/mamba"
-$MAMBA_EXE shell hook --shell fish --root-prefix $MAMBA_ROOT_PREFIX | source
-# <<< mamba initialize <<<
