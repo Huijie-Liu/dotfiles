f745ec12dc0e2ef74d8a39639bdf714d38a456a3
liuhuijie <huijieliu@buaa.edu.cn>
2026-01-21 15:15:34 +0800

    clean up fish config

--- .config/fish/conf.d/alias.fish
+++ .config/fish/conf.d/alias.fish
@@ -1,24 +1,5 @@
-# Basic
-abbr --add c clear
 abbr --add v nvim
 abbr --add ls 'lsd --group-directories-first'
 abbr --add l 'lsd -l --group-directories-first'
 abbr --add ll 'lsd -lA --group-directories-first'
 abbr --add la 'lsd -A --group-directories-first'
-abbr --add cd z
-abbr --add md "mkdir -p"
-abbr --add rd rmdir
-
-# Homebrew
-abbr --add bi "brew install"
-abbr --add bu "brew uninstall"
-abbr --add bc "brew cleanup --prune=all"
-abbr --add bU "brew update && brew upgrade && brew cleanup --prune=all"
-
-# Tool
-abbr --add zj zellij
-abbr --add lg lazygit
-abbr --add ff fastfetch
-abbr --add one onefetch
-abbr --add mm micromamba
-abbr --add 7z 7zz
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,1 +1,5 @@
 set fish_greeting
+
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init2.fish 2>/dev/null || :
