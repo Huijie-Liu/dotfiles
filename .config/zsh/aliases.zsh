e6a855c6cbf67e474bb24d0b92abca987a9c5bce
liuhuijie <huijieliu@buaa.edu.cn>
2026-01-21 15:26:08 +0800

    update zsh config

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -6,38 +6,7 @@ alias la='lsd -A'
 alias lt='lsd --tree --depth 2'
 alias lta='lsd --tree --depth 2 -a'
 
-# 目录导航
 alias ..='cd ..'
 alias ...='cd ../..'
 
-# 文件操作
-alias md='mkdir -p'
-alias rd='rmdir'
-
-# Tmux 控制
-alias ta='tmux attach'
-alias tl='tmux list-session'
-alias ts='tmux new-session'
-
-# Zellij 控制
-alias zj='zellij'
-alias za='zellij attach'
-alias zl='zellij list-sessions'
-alias zs='zellij --session'
-alias zk='zellij kill-session'
-alias zka='zellij kill-all-sessions'
-alias zjl='zellij --layout'
-
-# 终端控制 
-alias c='clear'
-alias r='source ~/.zshrc'
-alias e='nvim ~/.zshrc'
 alias v='nvim'
-
-# 自定义命令 
-alias nvs='nvidia-smi'
-alias lg='lazygit'
-alias ff='fastfetch'
-alias mm='micromamba'
-alias one='onefetch'
-alias nix='export IN_NIX_USER_CHROOT=1; export NP_RUNTIME=bwrap; ~/.nix-portable/nix-portable nix develop ~/.dotfiles'
--- .config/zsh/plugins.zsh
+++ .config/zsh/plugins.zsh
@@ -1,6 +1,6 @@
 eval "$(atuin init zsh)"
 
-eval "$(starship init zsh)"
+# eval "$(starship init zsh)"
 
 eval "$(zoxide init zsh)"
 
